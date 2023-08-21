a3602f8dec5579426ba193fc013c5d50607df6d4
Bruce <recall4056@gmail.com>
2023-08-21 08:53:53 +0800

    chore: update

--- packages/create-tornado/src/index.ts
+++ packages/create-tornado/src/index.ts
@@ -1,11 +1,79 @@
+import { blue, cyan, green, yellow } from 'kolorist'
 import prompts from 'prompts'
 
-const test = async () =>
+type ColorFunc = (str: string | number) => string
+
+type Framework = {
+  name: string
+  display: string
+  color: ColorFunc
+  variants: FrameworkVariant[]
+}
+
+const FRAMEWORKS: Framework[] = [
+  {
+    name: 'vue',
+    display: 'Vue',
+    color: green,
+    variants: [
+      {
+        name: 'vue-ts',
+        display: 'TypeScript',
+        color: blue
+      },
+      {
+        name: 'vue',
+        display: 'JavaScript',
+        color: yellow
+      }
+    ]
+  },
+  {
+    name: 'react',
+    display: 'React',
+    color: cyan,
+    variants: [
+      {
+        name: 'react-ts',
+        display: 'TypeScript',
+        color: blue
+      },
+      {
+        name: 'react',
+        display: 'JavaScript',
+        color: yellow
+      }
+    ]
+  }
+]
+
+type FrameworkVariant = {
+  name: string
+  display: string
+  color: ColorFunc
+  customCommand?: string
+}
+
+const enterProjectName = async () =>
   prompts({
-    type: 'number',
-    name: 'value',
-    message: 'Project Name:',
-    validate: (value) => (value < 18 ? 'Nightclub is 18+ only' : true)
+    type: 'text',
+    name: 'projectName',
+    message: 'Project Name:'
   })
 
-test()
+const enterSelectFramework = async () =>
+  prompts({
+    type: 'select',
+    name: 'framework',
+    message: 'Select Framework:',
+    choices: FRAMEWORKS.map((framework) => {
+      const frameworkColor = framework.color
+      return {
+        title: frameworkColor(framework.display || framework.name),
+        value: framework
+      }
+    })
+  })
+
+await enterProjectName()
+await enterSelectFramework()
